d17eedd855d149faa562e3c3fb0f81bca47ab8fb
Tomasz_Bajak <125197564+TBajak@users.noreply.github.com>
2024-01-24 20:03:47 +0100

    Delete Create 'Sieć Kin' directory

--- Create 'Sieć Kin'/Pliki SQL z Kodem Źródłowym/10 (Insert into cinema_halls_seats).sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO cinema_halls_seats (id_rooms, seat_number, "row")
-VALUES (1, 5, 1) ;
--- Create 'Sieć Kin'/Pliki SQL z Kodem Źródłowym/20 (Update more records).sql
+++
@@ -1,5 +0,0 @@
-UPDATE film_screening
-SET normal_ticket = 50,
-	discount_ticket = 19.84,
-	hours_of_screening = '22:22'
-WHERE id = 8; 
